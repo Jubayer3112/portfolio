0d7c33353cbb7b889a71c5688431ccf995687329
jubayer3112 <you@example.com>
2023-12-12 10:46:42 +0600

    navbar added

---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+const Navbar = () => {
+  const navigation = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>About</a>
+      </li>
+      <li>
+        <a>Resume</a>
+      </li>
+      <li>
+        <a>Services</a>
+      </li>
+      <li>
+        <a>Contact</a>
+      </li>
+    </>
+  );
+  return (
+    <div>
+      <div className="container mx-auto">
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 text-xl rounded-box w-52"
+              >
+                {navigation}
+              </ul>
+            </div>
+            <a className="text-3xl">Jubayer Hossain</a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 text-xl">
+                {navigation}
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <a className="py-2 px-10 bg-[#363062] text-white uppercase text-md">Hire Me!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
